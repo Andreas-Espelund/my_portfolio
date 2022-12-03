506eb06e494e007a2b5b986ae93e99fd7fcbd8e6
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2022-12-03 02:20:41 +0100

    chip color fix

--- app/components/TypeChip.tsx
+++ app/components/TypeChip.tsx
@@ -3,9 +3,20 @@ import React from 'react'
 export default function TypeChip({ type, size }: { type:string, size:string }) {
         
   const textSize = size === "large" ? "text-xl px-4 py-2" : ""
+  let background = "bg-primary"
   
+  switch (type) {
+    case "android":
+      background = "bg-android"
+      break;
+    case "web":
+      background = "bg-web"
+      break;
+    default:
+      break;
+  }
   return (
-    <div className={`bg-${type} w-fit text-white rounded-full shadow-lg px-2 block text-center font-semibold ${textSize}`}>
+    <div className={`${background} w-fit text-white rounded-full shadow-lg px-2 block text-center font-semibold ${textSize}`}>
         {type}
     </div>
   )
