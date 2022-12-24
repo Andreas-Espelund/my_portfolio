2903155790f1e2ac1d6995c591f258449d2b09ee
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2022-12-24 09:18:16 +0100

    tech row fit

--- app/components/Technologies.tsx
+++ app/components/Technologies.tsx
@@ -5,9 +5,9 @@ export default function Technologies({items, small}) {
     const cls =  small ?  'relative w-8 h-8' : 'relative w-8 h-8 lg:w-14 lg:h-14 flex flex-col items-center '
     const txt = small ? 'hidden' : 'mt-14'
     return (
-    <div className="flex gap-4 w-full">
+    <div className="w-full grid grid-flow-col place-content-start gap-1 lg:gap-6">
         {items.map((t: string) =>
-        <div className={`${cls} text-transparent font-semibold hover:text-inherit transition-all`}>
+        <div className={`${cls} text-transparent w-fit font-semibold hover:text-inherit transition-all`}>
             <Image src={getIcon(t)} fill alt={''}/>
             <p className={txt}>{t}</p>
         </div>
